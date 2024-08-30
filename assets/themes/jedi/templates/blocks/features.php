44504067c282abb7e8716beada18bd48fbd123e3
Vitaly Reutsky <79563766+vitalyreutsky@users.noreply.github.com>
2024-08-30 08:00:56 +0300

    fix features block

--- assets/themes/jedi/templates/blocks/features.php
+++ assets/themes/jedi/templates/blocks/features.php
@@ -7,21 +7,23 @@ $cards = get_field('cards');
    <div class="container">
       <div class="features__wrapper">
          <?php if ($title) : ?>
-            <h2 class="features__title"><?php echo $title; ?></h2>
+            <h2 class="features__title"><?php echo esc_html($title); ?></h2>
          <?php endif; ?>
 
-         <div class="features__cards">
-            <?php foreach ($cards as $card) : ?>
-               <div class="features__card">
-                  <div class="features__card-icon">
-                     <?php echo wp_get_attachment_image($card['icon'], 'full'); ?>
+         <?php if (!empty($cards)) : ?>
+            <div class="features__cards">
+               <?php foreach ($cards as $card) : ?>
+                  <div class="features__card">
+                     <div class="features__card-icon">
+                        <?php echo wp_get_attachment_image($card['icon'], 'full'); ?>
+                     </div>
+                     <div class="features__card-content">
+                        <?php echo wp_kses_post($card['content']); ?>
+                     </div>
                   </div>
-                  <div class="features__card-content">
-                     <?php echo $card['content']; ?>
-                  </div>
-               </div>
-            <?php endforeach; ?>
-         </div>
+               <?php endforeach; ?>
+            </div>
+         <?php endif; ?>
       </div>
    </div>
 </section>
